03a526310ce2ea8027965f885c6d4e95533e2d34
Chris Tomlinson <ct@moonvine.org>
2019-06-03 15:10:04 -0500

    first draft of a common set of functions across tools

---
+++ src/main/java/io/bdrc/libraries/GitHelpers.java
@@ -0,0 +1,105 @@
+package io.bdrc.libraries;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.errors.NoWorkTreeException;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
+import org.eclipse.jgit.api.Status;
+
+public class GitHelpers {
+
+    public static final String gitignore = "# Ignore everything\n" + 
+            "*\n" + 
+            "# Don't ignore directories, so we can recurse into them\n" + 
+            "!*/\n" + 
+            "# Don't ignore .gitignore and *.foo files\n" + 
+            "!.gitignore\n" + 
+            "!*.trig\n" + 
+            "";
+
+    public static void createDirIfNotExists(String dir) {
+        File theDir = new File(dir);
+        if (!theDir.exists()) {
+            //System.out.println("creating directory: " + dir);
+            try{
+                theDir.mkdir();
+            }
+            catch(SecurityException se){
+                System.err.println("could not create directory, please fasten your seat belt");
+            }
+        }
+    }
+    
+    public static Map<String, Repository> typeRepo = new HashMap<>();
+    
+    public static void ensureGitRepo(String type, String REPOS_BASE_DIR) {
+        if (typeRepo.containsKey(type))
+            return;
+        String dirpath = REPOS_BASE_DIR+type+'s';
+        createDirIfNotExists(dirpath);
+        FileRepositoryBuilder builder = new FileRepositoryBuilder();
+        File gitDir = new File(dirpath+"/.git");
+        File wtDir = new File(dirpath);
+        try {
+            Repository repository = builder.setGitDir(gitDir)
+              .setWorkTree(wtDir)
+              //.setMustExist( true )
+              .readEnvironment() // scan environment GIT_* variables
+              .build();
+            if (!repository.getObjectDatabase().exists()) {
+                System.out.println("create git repository in "+dirpath);
+                repository.create();
+                PrintWriter out = new PrintWriter(dirpath+".gitignore");
+                out.println(gitignore);
+                out.close();
+            }
+            typeRepo.put(type, repository);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public static Set<String> getChanges(String type) {
+        Repository r = typeRepo.get(type); 
+        if (r == null)
+            return null;
+        Git git = new Git(r);
+        Status status;
+        Set<String> res = new HashSet<>();
+        try {
+            status = git.status().call();
+        } catch (NoWorkTreeException | GitAPIException e) {
+            e.printStackTrace();
+            git.close();
+            return null;
+        }
+        res.addAll(status.getModified());
+        res.addAll(status.getAdded());
+        git.close();
+        return res;
+    }
+
+    public static void commitChanges(String type, String commitMessage) {
+        Repository r = typeRepo.get(type); 
+        if (r == null)
+            return;
+        Git git = new Git(r);
+        try {
+            git.add().addFilepattern(".").call();
+            git.commit().setMessage(commitMessage).call();
+        } catch (GitAPIException e) {
+            e.printStackTrace();
+        }
+        git.close();
+    }
+    
+}
