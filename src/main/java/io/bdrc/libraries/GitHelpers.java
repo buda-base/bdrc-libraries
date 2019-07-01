680c9d0ee6964eaa7d8a4320af23a943777e5033
Marc Agate <agate.marc@gmail.com>
2019-07-01 11:40:59 -0400

    preparing v 0.4.0 - now returning RevCommit

--- src/main/java/io/bdrc/libraries/GitHelpers.java
+++ src/main/java/io/bdrc/libraries/GitHelpers.java
@@ -9,56 +9,48 @@ import java.util.Map;
 import java.util.Set;
 
 import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.Status;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.errors.NoWorkTreeException;
 import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
-import org.eclipse.jgit.api.Status;
 
 public class GitHelpers {
 
-    public static final String gitignore = "# Ignore everything\n" + 
-            "*\n" + 
-            "# Don't ignore directories, so we can recurse into them\n" + 
-            "!*/\n" + 
-            "# Don't ignore .gitignore and *.foo files\n" + 
-            "!.gitignore\n" + 
-            "!*.trig\n" + 
-            "";
+    public static final String gitignore = "# Ignore everything\n" + "*\n" + "# Don't ignore directories, so we can recurse into them\n" + "!*/\n" + "# Don't ignore .gitignore and *.foo files\n" + "!.gitignore\n" + "!*.trig\n" + "";
 
     public static void createDirIfNotExists(String dir) {
         File theDir = new File(dir);
         if (!theDir.exists()) {
-            //System.out.println("creating directory: " + dir);
-            try{
+            // System.out.println("creating directory: " + dir);
+            try {
                 theDir.mkdir();
-            }
-            catch(SecurityException se){
+            } catch (SecurityException se) {
                 System.err.println("could not create directory, please fasten your seat belt");
             }
         }
     }
-    
+
     public static Map<String, Repository> typeRepo = new HashMap<>();
-    
+
     public static void ensureGitRepo(String type, String REPOS_BASE_DIR) {
         if (typeRepo.containsKey(type))
             return;
-        String dirpath = REPOS_BASE_DIR+type+'s';
+        String dirpath = REPOS_BASE_DIR + type + 's';
         createDirIfNotExists(dirpath);
         FileRepositoryBuilder builder = new FileRepositoryBuilder();
-        File gitDir = new File(dirpath+"/.git");
+        File gitDir = new File(dirpath + "/.git");
         File wtDir = new File(dirpath);
         try {
-            Repository repository = builder.setGitDir(gitDir)
-              .setWorkTree(wtDir)
-              //.setMustExist( true )
-              .readEnvironment() // scan environment GIT_* variables
-              .build();
+            Repository repository = builder.setGitDir(gitDir).setWorkTree(wtDir)
+                    // .setMustExist( true )
+                    .readEnvironment() // scan environment GIT_* variables
+                    .build();
             if (!repository.getObjectDatabase().exists()) {
-                System.out.println("create git repository in "+dirpath);
+                System.out.println("create git repository in " + dirpath);
                 repository.create();
-                PrintWriter out = new PrintWriter(dirpath+".gitignore");
+                PrintWriter out = new PrintWriter(dirpath + ".gitignore");
                 out.println(gitignore);
                 out.close();
             }
@@ -67,9 +59,9 @@ public class GitHelpers {
             e.printStackTrace();
         }
     }
-    
+
     public static Set<String> getChanges(String type) {
-        Repository r = typeRepo.get(type); 
+        Repository r = typeRepo.get(type);
         if (r == null)
             return null;
         Git git = new Git(r);
@@ -88,18 +80,20 @@ public class GitHelpers {
         return res;
     }
 
-    public static void commitChanges(String type, String commitMessage) {
-        Repository r = typeRepo.get(type); 
+    public static RevCommit commitChanges(String type, String commitMessage) {
+        Repository r = typeRepo.get(type);
+        RevCommit rev = null;
         if (r == null)
-            return;
+            return null;
         Git git = new Git(r);
         try {
             git.add().addFilepattern(".").call();
-            git.commit().setMessage(commitMessage).call();
+            rev = git.commit().setMessage(commitMessage).call();
         } catch (GitAPIException e) {
             e.printStackTrace();
         }
         git.close();
+        return rev;
     }
-    
+
 }
