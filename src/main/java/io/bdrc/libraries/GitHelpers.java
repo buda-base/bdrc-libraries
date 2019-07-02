bd5a84f0864892982fb0d09cc7b04cce688e5b66
Marc Agate <agate.marc@gmail.com>
2019-07-02 10:02:13 -0400

    extended GitHelpers

--- src/main/java/io/bdrc/libraries/GitHelpers.java
+++ src/main/java/io/bdrc/libraries/GitHelpers.java
@@ -9,12 +9,24 @@ import java.util.Map;
 import java.util.Set;
 
 import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.PullResult;
 import org.eclipse.jgit.api.Status;
+import org.eclipse.jgit.api.errors.CanceledException;
 import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.api.errors.InvalidConfigurationException;
+import org.eclipse.jgit.api.errors.InvalidRemoteException;
+import org.eclipse.jgit.api.errors.NoHeadException;
+import org.eclipse.jgit.api.errors.RefNotAdvertisedException;
+import org.eclipse.jgit.api.errors.RefNotFoundException;
+import org.eclipse.jgit.api.errors.TransportException;
+import org.eclipse.jgit.api.errors.WrongRepositoryStateException;
 import org.eclipse.jgit.errors.NoWorkTreeException;
 import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.lib.TextProgressMonitor;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
+import org.eclipse.jgit.transport.PushResult;
+import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
 
 public class GitHelpers {
 
@@ -94,6 +106,35 @@ public class GitHelpers {
         }
         git.close();
         return rev;
+    }
+
+    public static Repository getLocalRepo(String type) {
+        return typeRepo.get(type);
+    }
+
+    public static PullResult pull(String type, String REPOS_BASE_DIR)
+            throws WrongRepositoryStateException, InvalidConfigurationException, InvalidRemoteException, CanceledException, RefNotFoundException, RefNotAdvertisedException, NoHeadException, TransportException, GitAPIException {
+        GitHelpers.ensureGitRepo(type, REPOS_BASE_DIR);
+        Git git = new Git(getLocalRepo(type));
+        PullResult res = git.pull().setProgressMonitor(new TextProgressMonitor()).setRemote("origin").call();
+        git.close();
+        return res;
+    }
+
+    public static Iterable<PushResult> push(String type, String REMOTE_BASE_URL, UsernamePasswordCredentialsProvider prov, String REPOS_BASE_DIR) throws InvalidRemoteException, TransportException, GitAPIException {
+        GitHelpers.ensureGitRepo(type, REPOS_BASE_DIR);
+        Git git = new Git(getLocalRepo(type));
+        Iterable<PushResult> res = git.push().setCredentialsProvider(prov).setRemote(REMOTE_BASE_URL + "/" + type + "s").call();
+        git.close();
+        return res;
+    }
+
+    public static Iterable<PushResult> push(String type, String REMOTE_BASE_URL, String user, String pass, String REPOS_BASE_DIR) throws InvalidRemoteException, TransportException, GitAPIException {
+        GitHelpers.ensureGitRepo(type, REPOS_BASE_DIR);
+        Git git = new Git(getLocalRepo(type));
+        Iterable<PushResult> res = git.push().setCredentialsProvider(new UsernamePasswordCredentialsProvider(user, pass)).setRemote(REMOTE_BASE_URL + "/" + type + "s").call();
+        git.close();
+        return res;
     }
 
 }
