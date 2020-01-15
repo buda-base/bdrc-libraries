9f279e0a9ebd222faf5ae7dc64e9a1d1ce0108f3
Marc Agate <agate.marc@gmail.com>
2020-01-15 16:16:29 -0400

    using javaMail for email notification

--- src/main/java/io/bdrc/libraries/EmailNotification.java
+++ src/main/java/io/bdrc/libraries/EmailNotification.java
@@ -3,27 +3,40 @@ package io.bdrc.libraries;
 import java.util.ArrayList;
 import java.util.Properties;
 
-import net.sargue.mailgun.Configuration;
-import net.sargue.mailgun.Mail;
-import net.sargue.mailgun.MailBuilder;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 public class EmailNotification {
 
-    private Mail mail;
+    Message message;
 
-    public EmailNotification(String content, String fromEmail, String fromName, String subject, Properties mailProps, ArrayList<String> recipients) {
-        // TODO Auto-generated constructor stub
-        Configuration configuration = new Configuration().domain(mailProps.getProperty("mail.domain")).apiKey(mailProps.getProperty("mail.key"));
-        MailBuilder build = Mail.using(configuration);
-        for (String s : recipients) {
-            build = build.to(s);
+    public EmailNotification(String content, String fromEmail, String fromName, String subject, Properties prop, ArrayList<String> recipients) throws AddressException, MessagingException {
+
+        Session session = Session.getInstance(prop, new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(prop.getProperty("emailUser"), prop.getProperty("emailPassword"));
+            }
+        });
+        String rList = "";
+        for (String ad : recipients) {
+            rList = rList + ad + ",";
         }
-        build.from(fromName, fromEmail).replyTo(fromEmail).subject(subject).text(content);
-        this.mail = build.build();
+        rList = rList.substring(0, rList.length() - 1);
+        this.message = new MimeMessage(session);
+        this.message.setFrom(new InternetAddress(fromEmail));
+        this.message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(rList));
+        this.message.setSubject(subject);
+        this.message.setText(content);
     }
 
-    public void send() {
-        this.mail.send();
+    public void send() throws MessagingException {
+        Transport.send(this.message);
     }
 
 }
